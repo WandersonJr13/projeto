c5797b46954425fda4a51076e3520de333445c08
Wanderson Campos <wanderson.junior@mediclick.com.br>
2023-04-18 13:17:11 -0300

    Criando os relacionamentos do banco de dados

--- apps/registro_hora_extra/admin.py
+++ apps/registro_hora_extra/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import RegistroHoraExtra
+from apps.registro_hora_extra .models import RegistroHoraExtra
 
 
 admin.site.register(RegistroHoraExtra)
+
+
--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from apps.funcionarios.models import Funcionario
 
 
 class RegistroHoraExtra(models.Model):
-    motivo = models.CharField(max_length=100)
+    motivo = models.CharField(max_length=100, help_text='motivo')
+    funcionario = models.ForeignKey(Funcionario, models.PROTECT)
+
 
     def __str__(self):
         return self.motivo
